5abc61166ce79234805398c20d753a7e1f4c65b3
Adam Cavaliere <adam@mbp.local>
2018-03-17 08:58:14 -0500

    fixed description

--- application-config/variables.tf
+++ application-config/variables.tf
@@ -1,26 +1,26 @@
 variable "app_name" {
-  Description = "Application Name"
+  description = "Application Name"
   default     = "notSet"
 }
 
 variable "owner" {
-  Description = "Owner of the instances"
+  description = "Owner of the instances"
   default     = "notSet"
 }
 
 variable "ttl" {
-  Description = "Time to live"
+  description = "Time to live"
   default     = "notSet"
 }
 
 variable "keyname" {
-  Description = "Key name for loggin into the instance"
+  description = "Key name for loggin into the instance"
 }
 
 variable "organization" {
-  Description = "Organization in TFE"
+  description = "Organization in TFE"
 }
 
 variable "workspace" {
-  Description = "Network Workspace"
+  description = "Network Workspace"
 }
