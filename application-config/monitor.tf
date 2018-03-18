c460a4137167d9be71589b68b3a4d74050ab713f
Adam Cavaliere <adam@mbp.local>
2018-03-18 08:50:10 -0500

    datadog config

--- application-config/monitor.tf
+++ application-config/monitor.tf
@@ -1,6 +1,6 @@
 data "template_file" "hostlist" {
   count    = 2
-  template = "$${prefix}$${actualhost}!"
+  template = "$${prefix}$${actualhost}"
 
   vars {
     prefix     = "host:"
@@ -8,6 +8,36 @@ data "template_file" "hostlist" {
   }
 }
 
-output "rendered" {
+output "datadog_host_list" {
   value = "${join(",", data.template_file.hostlist.*.rendered)}"
+}
+
+provider "datadog" {
+  api_key = "${var.datadog_api_key}"
+  app_key = "${var.datadog_app_key}"
+}
+
+resource "datadog_monitor" "app" {
+  name  = "${var.app_name} Host Monitor"
+  type  = "service check"
+  query = "datadog.agent.up.over(${join(",", data.template_file.hostlist.*.rendered)}).last(2).count_by_status()"
+
+  thresholds {
+    ok                = 0
+    warning           = 2
+    warning_recovery  = 1
+    critical          = 4
+    critical_recovery = 3
+  }
+
+  notify_no_data    = false
+  renotify_interval = 60
+
+  notify_audit = false
+  timeout_h    = 60
+  include_tags = true
+
+  silenced {
+    "*" = 0
+  }
 }
